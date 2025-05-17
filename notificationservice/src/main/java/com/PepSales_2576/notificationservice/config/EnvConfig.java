cf72fb80568317eeac0c4c08cca8fe3653dfe3bc
Raja Kumar Rana <164044997+raja2576@users.noreply.github.com>
2025-05-18 01:03:02 +0530

    Update EnvConfig.java

--- notificationservice/src/main/java/com/PepSales_2576/notificationservice/config/EnvConfig.java
+++ notificationservice/src/main/java/com/PepSales_2576/notificationservice/config/EnvConfig.java
@@ -1,18 +1,12 @@
 package com.PepSales_2576.notificationservice.config;
 
-
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EnvConfig {
-    private static Dotenv dotenv = Dotenv.load();
 
     public static String getEnv(String key) {
         String value = System.getenv(key);
-        if (value == null) {
-            value = dotenv.get(key);
-        }
         if (value == null) {
             System.err.println("WARNING: Environment variable '" + key + "' is not set.");
         }
@@ -20,4 +14,3 @@ public class EnvConfig {
     }
 
 }
-
